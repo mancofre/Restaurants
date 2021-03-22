5dc6e634eef9cc2138bf8bc371dc63b18a0ef3db
mancofre <73198514+mancofre@users.noreply.github.com>
2021-03-22 17:55:48 -0300

    Pantalla Restaurant

--- navigations/RestaurantsStack.js
+++ navigations/RestaurantsStack.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Restaurants from '../screens/Restaurants';
+import Restaurants from '../screens/restaurants/Restaurants';
 
 const Stack = createStackNavigator();
 
 export default function RestaurantsStack() {
+    
     return (
         <Stack.Navigator>
             <Stack.Screen 
